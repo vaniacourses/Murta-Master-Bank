5f431259c4cc96bb52d1305290bc572620b4ce2d
Anderson Gomes <derson3001@gmail.com>
2026-06-04 00:09:04 -0300

    feat: trata erros (antes só dava 403)

---
+++ backendMMBank/src/main/java/br/uff/ic/mmbank/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package br.uff.ic.mmbank.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgument(IllegalArgumentException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST) // Retorna 400
+                .body(Map.of("erro", ex.getMessage()));
+    }
+}
