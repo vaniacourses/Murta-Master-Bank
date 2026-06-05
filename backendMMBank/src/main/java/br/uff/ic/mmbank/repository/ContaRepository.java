d99373778135f93ae107d067030c2ac5725213db
Paulo Jorge Campos Cardoso <pjcardoso25@gmail.com>
2026-06-05 14:22:38 -0300

    feat: add pessimistic locking methods for Conta entity in repository

--- backendMMBank/src/main/java/br/uff/ic/mmbank/repository/ContaRepository.java
+++ backendMMBank/src/main/java/br/uff/ic/mmbank/repository/ContaRepository.java
@@ -5,6 +5,7 @@ import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -14,4 +15,10 @@ public interface ContaRepository extends JpaRepository<Conta, Long> { // Tempor√
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT c FROM Conta c WHERE c.id = :id")
     Optional<Conta> findByIdForUpdate(Long id);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT c FROM Conta c WHERE c.numeroConta = :numeroConta")
+    Optional<Conta> findByNumeroContaForUpdate(@Param("numeroConta") String numeroConta);
+
+    Optional<Conta> findByNumeroConta(String numeroConta);
 }
