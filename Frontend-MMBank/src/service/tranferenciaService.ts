769ed38f399e3868869efeae6b692e9627eca481
Anderson Gomes <derson3001@gmail.com>
2026-06-08 23:12:35 -0300

    feat: integração com frontend

--- Frontend-MMBank/src/service/tranferenciaService.ts
+++ Frontend-MMBank/src/service/tranferenciaService.ts
@@ -1,25 +1,18 @@
+import { api } from './api';
 import type { TransferenciaRequestDTO, TransferenciaResponseDTO } from '../types/dtos/index';
 
 export const transferenciaService = {
-  realizar: async (dados: TransferenciaRequestDTO): Promise<TransferenciaResponseDTO> => {
-    // Simulação do backend enquanto a API Spring Boot não está ligada
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (dados.valor <= 0) {
-          reject(new Error("O valor deve ser maior que zero."));
-          return;
-        }
-        resolve({
-          id: Math.random().toString(36).substring(7),
-          dataHora: new Date().toISOString(),
-          status: 'CONCLUIDA',
-          comprovante: 'TRX-' + Math.floor(Math.random() * 10000)
-        });
-      }, 1500);
-    });
+  
+  // rota de PIX
+  realizarPix: async (dados: TransferenciaRequestDTO): Promise<TransferenciaResponseDTO> => {
+    const response = await api.post<TransferenciaResponseDTO>('/transferencias/pix', dados);
+    return response.data;
+  },
 
-    // Código real futuro:
-    // const response = await api.post<TransferenciaResponseDTO>('/transferencias', dados);
-    // return response.data;
+  // rota de transferencia padrão (TED)
+  realizar: async (dados: TransferenciaRequestDTO): Promise<TransferenciaResponseDTO> => {
+    const response = await api.post<TransferenciaResponseDTO>('/transferencias', dados);
+    return response.data;
   }
+  
 };
